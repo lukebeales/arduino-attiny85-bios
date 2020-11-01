48f2a1df2ee80681df28828fcfb3106cd0bad199
Luke Beales <lukebeales@gmail.com>
2020-11-01 20:06:27 +1000

    renaming variables to make more sense

--- map.cpp
+++ map.cpp
@@ -13,38 +13,40 @@ char settings_buffer[64];
 // this writes the specific setting to the eeprom, rearranging settings as it goes
 bool settings_write() {
    
-  int map_size = eeprom[0] - 48;    // the 48 shift allows us to count starting at the ascii code of 0, so up to 10 settings will seem normal at least
-  int map_start = 1;                // where in the map the setting will sit
-  int map_length = 0;               // how big the map is currently
+  int old_map_size = eeprom[0] - 48;    // the 48 shift allows us to count starting at the ascii code of 0, so up to 10 settings will seem normal at least
+  int old_map_offeset = 1;                // where in the map the setting will sit
+  int old_map_length = 0;               // how big the map entry is currently
 
-  char map_data[2];
+  char new_map_data[2];
   // if there's something to set
   if ( std::char_traits<char>::length(settings_buffer) > 0 ) {
-      map_data[0] = settings_code;
-      map_data[1] = std::char_traits<char>::length(settings_buffer) + 48;       // this seems to count correctly now
+      new_map_data[0] = settings_code;
+      new_map_data[1] = std::char_traits<char>::length(settings_buffer) + 48;       // this seems to count correctly now
   // otherwise we want to remove the setting right.
   } else {
-      map_data[0] = '\0';
+      new_map_data[0] = '\0';
   }
 
-  int data_start = 1 + map_size; // where the settings data will sit
-  int data_length = 0;
+  int old_data_offset = 1 + old_map_size; // where the settings data will sit
+  int old_data_length = 0;
 
+  int new_map_size = 0 + old_map_size;
+  
   // go through the map counting up the map_offset & data_offset as we go
-  for ( int i = 1; i < map_size; i += 2 ) {
+  for ( int i = 1; i < old_map_size; i += 2 ) {
 
         // if the current setting 'character' is the one we want, great!
         if ( eeprom[i] == settings_code ) {
      
             std::cout << "Found it to replace!\n";
 
-            map_start = i;                          // record where we are in the map
-            map_length = 2;                         // this is how big the current map entry is
-            data_length = (eeprom[i + 1] - 48);     // this is how much data is currently used by the existing setting
+            old_map_offset = i;                          // record where we are in the map
+            old_map_length = 2;                         // this is how big the current map entry is
+            old_data_length = (eeprom[i + 1] - 48);     // this is how much data is currently used by the existing setting
 
             // if we're looking to replace it with nothing
-            if ( std::char_traits<char>::length(map_data) == 0 ) {
-                map_size -= 2;  // reduce the map size by 2.
+            if ( std::char_traits<char>::length(new_map_data) == 0 ) {
+                new_map_size -= 2;  // reduce the map size by 2.
             }
 
             break;
@@ -54,12 +56,12 @@ bool settings_write() {
 
             std::cout << "New entry in the middle somewhere!\n";
 
-            map_start = i;
-            map_length = 2;
+            old_map_offset = i;
+            old_map_length = 2;
 
             // if there's data to insert...
-            if ( std::char_traits<char>::length(map_data) > 0 ) {
-                map_size += 2;  // increase the map size to accommodate the new data
+            if ( std::char_traits<char>::length(new_map_data) > 0 ) {
+                new_map_size += 2;  // increase the map size to accommodate the new data
             }
 
             break;
@@ -67,33 +69,33 @@ bool settings_write() {
         } else {
 
             std::cout << "Nothing matched, add up some numbers\n";
-            data_start += (eeprom[i + 1] - 48);  // allows us to use some ascii numbers to start with.
+            old_data_offset += (eeprom[i + 1] - 48);  // allows us to use some ascii numbers to start with.
 
         }
 
     // if we're the last run, it means we're a new instruction on the end.
-    if ( i == 1 + (map_size - 2) ) {
-        map_start = map_size;
+    if ( i == 1 + (old_map_size - 2) ) {
+        old_map_offset = old_map_size;
     }
 
   }
 
   // if we ended up with a new instruction on the end
-  if ( map_start == (1 + map_size) ) {
-      map_size += 2;
+  if ( old_map_offset == (1 + old_map_size) ) {
+      new_map_size += 2;
   }
 
-  std::cout << map_size;
+  std::cout << new_map_size;
   std::cout << "\n";
-  std::cout << map_start;
+  std::cout << old_map_offset;
   std::cout << "\n";
-  std::cout << map_length;
+  std::cout << old_map_length;
   std::cout << "\n";
-  std::cout << map_data;
+  std::cout << new_map_data;
   std::cout << "\n";
-  std::cout << data_start;
+  std::cout << old_data_offset;
   std::cout << "\n";
-  std::cout << data_length;
+  std::cout << old_data_length;
   std::cout << "\n";
   std::cout << settings_buffer;
   std::cout << "\n";
