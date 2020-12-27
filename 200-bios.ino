62ddfb11c8ff43780ff6257c9517d0d5bd8b771d
Luke Beales <lukebeales@gmail.com>
2020-12-27 12:32:42 +1000

    its working!!!

--- 200-bios.ino
+++ 200-bios.ino
@@ -18,7 +18,7 @@ char bios_buffer[64];
 // i know this wastes clock cycles, but they are free whereas my brain and the chips memory isn't!
 void bios_replace(int offset, int length) {
 
-    int old_map_size = (byte)EEPROM.read(0) - 48;  // size of the map partition
+    int old_map_size = (int)EEPROM.read(0) - 48;  // size of the map partition
     //c++ int difference = std::char_traits<char>::length(bios_buffer) - length;
     int difference = strlen(bios_buffer) - length;
 
@@ -32,7 +32,7 @@ void bios_replace(int offset, int length) {
         // go through the whole map, counting how large the data is
         // this is so we know how much to shift left by, incase the new value is less than the old value
         for ( int i = 1; i < old_map_size; i += 2 ) {
-            old_data_size += ((byte)EEPROM.read(i) + 1) - 48;
+            old_data_size += ((int)EEPROM.read(i) + 1) - 48;
         }
     }
    
@@ -41,7 +41,7 @@ void bios_replace(int offset, int length) {
 
         // shift all data to the right to make room
         for ( int i = 1 + old_map_size + old_data_size; i > offset + (length - difference); i-- ) {
-            EEPROM.write(i + difference, (byte)EEPROM.read(i));
+            EEPROM.write(i + difference, (int)EEPROM.read(i));
         }
 
     // else if the replacement data is SMALLER than the original data
@@ -49,7 +49,7 @@ void bios_replace(int offset, int length) {
 
         // shift all data to the left
         for ( int i = offset + (length + difference); i < 1 + old_map_size + old_data_size; i++ ) {
-            EEPROM.write(i, (byte)EEPROM.read(i - difference));
+            EEPROM.write(i, (int)EEPROM.read(i - difference));
         }
 
     }
@@ -68,7 +68,7 @@ void bios_replace(int offset, int length) {
 // this writes the specific setting to the eeprom, rearranging bios as it goes
 bool bios_write() {
    
-  int old_map_size = (byte)EEPROM.read(0) - 48;    // the 48 shift allows us to count starting at the ascii code of 0, so up to 10 bios will seem normal at least
+  int old_map_size = (int)EEPROM.read(0) - 48;    // the 48 shift allows us to count starting at the ascii code of 0, so up to 10 bios will seem normal at least
   int old_map_offset = 1;                // where in the map the setting will sit
   int old_map_length = 0;               // how big the map entry is currently
 
@@ -87,7 +87,7 @@ bool bios_write() {
 
             old_map_offset = i;                          // record where we are in the map
             old_map_length = 2;                         // this is how big the current map entry is
-            old_data_length = ((byte)EEPROM.read(i + 1) - 48);     // this is how much data is currently used by the existing setting
+            old_data_length = ((int)EEPROM.read(i + 1) - 48);     // this is how much data is currently used by the existing setting
 
             // if we're looking to replace it with nothing
             //c++ if ( std::char_traits<char>::length(bios_buffer) == 0 ) {
@@ -98,7 +98,7 @@ bool bios_write() {
             break;
 
         // otherwise if the current setting 'character' is after the one we want, then we didn't find it
-        } else if ( (byte)EEPROM.read(i) > (byte)bios_code ) {
+        } else if ( (int)EEPROM.read(i) > (int)bios_code ) {
 
             // std::cout << "New entry in the middle somewhere!\n";
 
@@ -115,7 +115,7 @@ bool bios_write() {
         } else {
 
             // std::cout << "Nothing matched, add up some numbers\n";
-            old_data_offset += ((byte)EEPROM.read(i + 1) - 48);  // allows us to use some ascii numbers to start with.
+            old_data_offset += ((int)EEPROM.read(i + 1) - 48);  // allows us to use some ascii numbers to start with.
 
         }
 
@@ -170,7 +170,7 @@ bool bios_read() {
   // reset the buffer just in case
   bios_buffer[0] = '\0';
 
-  int map_size = (byte)EEPROM.read(0) - 48;    // the 48 shift allows us to count starting at the ascii code of 0, so up to 10 settings will seem normal at least
+  int map_size = (int)EEPROM.read(0) - 48;    // the 48 shift allows us to count starting at the ascii code of 0, so up to 10 settings will seem normal at least
   int data_offset = 1 + map_size;   // where the setting data sits
 
   // go through the map counting up the map_offset & data_offset as we go
@@ -182,19 +182,19 @@ bool bios_read() {
         // std::cout << "Found it!\n";
 
         // set the buffer to the value
-        for ( int j = 0; j < (byte)EEPROM.read(i + 1) - 48; j++ ) {
+        for ( int j = 0; j < (int)EEPROM.read(i + 1) - 48; j++ ) {
 
             bios_buffer[j] = (char)EEPROM.read(data_offset + j);
         }
 
         // close the string
-        bios_buffer[((byte)EEPROM.read(i + 1) - 48)] = '\0';
+        bios_buffer[((int)EEPROM.read(i + 1) - 48)] = '\0';
 
         break;
 
       }
 
-      data_offset += (byte)EEPROM.read(i + 1) - 48;
+      data_offset += (int)EEPROM.read(i + 1) - 48;
   }
 
   return true;
@@ -205,8 +205,14 @@ bool bios_read() {
 // this sets the first bit as ascii 0.
 bool bios_reset() {
 
-    EEPROM.write(0, 0 + 48);
-    EEPROM.write(1, '\0');
+  serial.println("");
+  serial.print(F("!!! bios data failed check, formatting..."));
+
+  EEPROM.write(0, 0 + 48);
+  EEPROM.write(1, '\0');
+
+  serial.println(F("done !!!"));
+  serial.println("");
 
 }
 
@@ -220,41 +226,61 @@ bool bios_verify() {
 
   // if the memory looks clean already
   if ( (char)EEPROM.read(0) == '\0' ) {
+
       bios_reset();
+
   } else {
 
-      int map_size = (byte)EEPROM.read(0) - 48;    // the 48 shift allows us to count starting at the ascii code of 0, so up to 10 settings will seem normal at least
+      int map_size = (int)EEPROM.read(0) - 48;    // the 48 shift allows us to count starting at the ascii code of 0, so up to 10 settings will seem normal at least
       int data_size = 0;
     
-      // go through the map counting up the map_offset & data_offset as we go
-      for ( int i = 1; i < map_size; i += 2 ) {
-        
-            // if the eeprom entry is null
-            if ( ( (char)EEPROM.read(i) == '\0' ) || ( (char)EEPROM.read(i + 1) == '\0' ) ) {
-    
-                i = map_size;
-                data_size = 0;
-                bios_reset();
+      // if the map seems too large...
+      if ( map_size > 100 ) {
 
-            } else {
-                data_size += (byte)EEPROM.read(i + 1) - 48;  // add to the data size
-            }
+        bios_reset();
+
+      } else {
+
+        // go through the map counting up the map_offset & data_offset as we go
+        for ( int i = 1; i < map_size; i += 2 ) {
+          
+              // if the eeprom entry is null
+              if ( ( (char)EEPROM.read(i) == '\0' ) || ( (char)EEPROM.read(i + 1) == '\0' ) ) {
+      
+                  i = map_size;
+                  data_size = 0;
+                  bios_reset();
+  
+              } else {
+                  data_size += (int)EEPROM.read(i + 1) - 48;  // add to the data size
+              }
+        }
+
       }
 
       if ( data_size > 0 ) {
 
-        // go through the data to see if there's any \0's within it
-        for ( int i = 1 + map_size; i < 1 + map_size + data_size; i ++ ) {
-        
-            if ( (char)EEPROM.read(i) == '\0' ) {
-    
-                i = 1 + map_size + data_size;
-                bios_reset();
+        // if it seems like the values are wrong...
+        if ( ( 1 + data_size + data_size ) > 512 ) {
 
-            }
-            
+          bios_reset();
+
+        } else {
+  
+          // go through the data to see if there's any \0's within it
+          for ( int i = 1 + map_size; i < 1 + map_size + data_size; i ++ ) {
+  
+              if ( (char)EEPROM.read(i) == '\0' ) {
+      
+                  i = 1 + map_size + data_size;
+                  bios_reset();
+  
+              }
+              
+          }
+
         }
-          
+
       }
 
   }  
@@ -265,7 +291,7 @@ bool bios_verify() {
 // reads and prints all the settings.
 bool bios_read_all() {
 
-  int map_size = (byte)EEPROM.read(0) - 48;    // the 48 shift allows us to count starting at the ascii code of 0, so up to 10 settings will seem normal at least
+  int map_size = (int)EEPROM.read(0) - 48;    // the 48 shift allows us to count starting at the ascii code of 0, so up to 10 settings will seem normal at least
 
   // go through the map counting up the map_offset & data_offset as we go
   for ( int i = 1; i < map_size; i += 2 ) {
@@ -292,6 +318,9 @@ bool bios_init() {
   bios_verify();
   bios_read_all();
 
+  serial.println(F("==============================="));
+  serial.print(F("Type a setting to change: "));
+
   // give the user a rolling 5 seconds to change it
   unsigned int expires = round(millis() / 1000) + 5;
   while (
@@ -303,7 +332,7 @@ bool bios_init() {
 
     if ( serial_read() ) {
 
-      serial.print("buffer: ");
+      serial.print(F("buffer: "));
       serial.println(serial_buffer);
 
       // if something is received on the serial, recalculate the expires value
@@ -332,6 +361,9 @@ bool bios_init() {
     
       serial.println(bios_buffer);
   */
+
+  serial.println("");
+  serial.println(F("Exiting bios..."));
 
   serial_end();
 }
--- bios.ino
+++ bios.ino
@@ -1,11 +1,1 @@
-void setup() {
-
-  // put your setup code here, to run once:
-  bios_init();
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+// this file is mainly to keep the ide happy, but put any global vars in here.
